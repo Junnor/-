1e73647eeba7f7065886e1cc3d13540bd1dde54c
ZhuDengquan <dengquanzhu@gmail.com>
2017-05-17 17:54:27 +0800

    调整漫展详情视图

--- ShangWuMiao/ShangWuMiao/Controller/ExhibitionDetailViewController.swift
+++ ShangWuMiao/ShangWuMiao/Controller/ExhibitionDetailViewController.swift
@@ -8,31 +8,59 @@
 // navigation bar 的设置没弄好
 
 import UIKit
+import Kingfisher
 
-class ExhibitionDetailViewController: UIViewController, UIScrollViewDelegate {
+class ExhibitionDetailViewController: UIViewController {
     
     var exhibition: Exhibition!
     
-    @IBOutlet weak var scrollView: UIScrollView! {
+    @IBOutlet weak var priceLabel: UILabel!
+    
+    @IBOutlet weak var collectionView: UICollectionView! {
         didSet {
-            scrollView.delegate = self
-            scrollView.showsHorizontalScrollIndicator = false
+            collectionView.dataSource = self
+            collectionView.delegate = self
+            
+            collectionView.showsVerticalScrollIndicator = false
         }
     }
     
+    // MARK: - View controller lifecycle
+    
+    fileprivate let constCellCounts = 4
+    fileprivate var tickts = [Ticket]()
     override func viewDidLoad() {
         super.viewDidLoad()
-        setNaviView()
-        scrollView.contentSize = CGSize(width: view.bounds.width, height: 1000)
         
-        exhibition.requestExhibitionListTickets { (success, info, tickts) in
+        registerCollectionView()
+        setNaviView()
+        
+        exhibition.requestExhibitionListTickets { [weak self] (success, info, tickts) in
             if success {
-                print(".. success")
+                self?.tickts = tickts
+                self?.collectionView.reloadData()
             } else {
                 print("request exhibition ticket failure: \(info!)")
             }
-            
         }
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        self.navigationController?.navigationBar.setBackgroundImage(UIImage(), for: .default)
+        self.navigationController?.navigationBar.shadowImage = UIImage()
+    }
+
+    
+    // MARK: - Helper
+    
+    private func registerCollectionView() {
+        collectionView.register(UINib(nibName: "ExHeaderCell", bundle: nil), forCellWithReuseIdentifier: "ExHeaderCellID")
+        collectionView.register(UINib(nibName: "ExDescriptionCell", bundle: nil), forCellWithReuseIdentifier: "ExDescriptionCellID")
+        collectionView.register(UINib(nibName: "ExInputHintCell", bundle: nil), forCellWithReuseIdentifier: "ExInputHintCellID")
+        collectionView.register(UINib(nibName: "ExTicketCell", bundle: nil), forCellWithReuseIdentifier: "ExTicketCellID")
+        collectionView.register(UINib(nibName: "ExFooterView", bundle: nil), forSupplementaryViewOfKind: UICollectionElementKindSectionFooter, withReuseIdentifier: "ExFooterViewID")
     }
     
     private var naviView: CustomNaviBarView!
@@ -43,12 +71,9 @@ class ExhibitionDetailViewController: UIViewController, UIScrollViewDelegate {
         
         self.view.insertSubview(naviView, belowSubview: (navigationController?.navigationBar)!)
     }
-    
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        
-        self.navigationController?.navigationBar.setBackgroundImage(UIImage(), for: .default)
-        self.navigationController?.navigationBar.shadowImage = UIImage()
+
+    @IBAction func buy(_ sender: Any) {
+        print("... buy")
     }
     
     // set navigation bar
@@ -63,3 +88,133 @@ class ExhibitionDetailViewController: UIViewController, UIScrollViewDelegate {
         }
     }
 }
+
+extension ExhibitionDetailViewController: UICollectionViewDataSource {
+    
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return constCellCounts + self.tickts.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        if indexPath.row == 0 {
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "ExHeaderCellID", for: indexPath)
+            cell.backgroundColor = UIColor.white
+            if let cell = cell as? ExHeaderCell {
+                if let url = URL(string: kImageHeaderUrl + self.exhibition.cover!) {
+                    let resourcce = ImageResource(downloadURL: url, cacheKey: url.absoluteString)
+                    cell.exImageView.kf.setImage(with: resourcce,
+                                      placeholder: nil,
+                                      options: [.transition(.fade(1))],
+                                      progressBlock: nil,
+                                      completionHandler: nil)
+                }
+                
+                cell.nameLabel.text = self.exhibition.name
+                cell.presaleLabel.text = self.exhibition.presale_price
+                cell.scenePriceLabel.text = self.exhibition.scene_price
+                cell.locationLabel.text = self.exhibition.location
+                
+                let startTime = exhibition.exhibition(stringTime: self.exhibition.start_time, digit: true)
+                let endTime = exhibition.exhibition(stringTime: self.exhibition.end_time, digit: true)
+                
+                cell.timeLabel.text = "\(startTime) - \(endTime)"
+            }
+            
+            return cell
+        } else if indexPath.item == 1 {
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "ExDescriptionCellID", for: indexPath)
+            cell.backgroundColor = UIColor.white
+            if let cell = cell as? ExDescriptionCell {
+                cell.titleLabel.text = self.exhibition.exDescription
+            }
+            return cell
+        } else if indexPath.item == 2 {
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "ExDescriptionCellID", for: indexPath)
+            cell.backgroundColor = UIColor.lightGray
+            if let cell = cell as? ExDescriptionCell {
+                cell.titleLabel.text = self.exhibition.addr
+            }
+
+            cell.backgroundColor = UIColor.red
+
+            return cell
+        } else if indexPath.item == 3 {
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "ExInputHintCellID", for: indexPath)
+            cell.backgroundColor = UIColor.gray
+            
+            return cell
+        }  else  {
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "ExTicketCellID", for: indexPath)
+            cell.backgroundColor = UIColor.white
+            if let cell = cell as? ExTicketCell {
+                let tickt = tickts[indexPath.item - constCellCounts]
+                cell.nameLabel.text = tickt.name
+                cell.priceLabel.text = tickt.price
+            }
+            
+            return cell
+        }
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
+        let footerView = collectionView.dequeueReusableSupplementaryView(ofKind: UICollectionElementKindSectionFooter, withReuseIdentifier: "ExFooterViewID", for: indexPath)
+        
+        if let footer = footerView as? ExFooterView, indexPath.section == 0 {
+            footer.plusButton.addTarget(self, action: #selector(plusAction), for: .touchUpInside)
+            footer.minusButton.addTarget(self, action: #selector(minusAction), for: .touchUpInside)
+
+        }
+        return footerView
+    }
+    
+    @objc private func plusAction() {
+        print("... plus")
+    }
+    
+    @objc private func minusAction() {
+        print("... minus")
+    }
+    
+}
+
+extension ExhibitionDetailViewController: UICollectionViewDelegateFlowLayout {
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 0
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 0
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForFooterInSection section: Int) -> CGSize {
+        return CGSize(width: collectionView.bounds.width, height: 60)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let width = collectionView.bounds.width
+        var height: CGFloat = 0.0
+        switch indexPath.row {
+        case 0:
+            height = width * 220 / 375
+        case 1:
+            let font = UIFont(name: "AvenirNext-Regular", size: 13)!
+            let str = NSString(string: self.exhibition.exDescription)
+            let rect = str.boundingRect(with: CGSize(width: width - 20, height: CGFloat(MAXFLOAT)), options: .usesLineFragmentOrigin, attributes: [NSFontAttributeName: font], context: nil)
+            height = ceil(rect.height)
+        case 2:
+            let font = UIFont(name: "AvenirNext-Regular", size: 13)!
+            let str = NSString(string: self.exhibition.addr)
+            let rect = str.boundingRect(with: CGSize(width: width - 20, height: CGFloat(MAXFLOAT)), options: .usesLineFragmentOrigin, attributes: [NSFontAttributeName: font], context: nil)
+            height = ceil(rect.height)        case 3:
+            height = 120
+        default:
+            height = 50
+            break
+        }
+        
+        return CGSize(width: width, height: height)
+    }
+}
+
